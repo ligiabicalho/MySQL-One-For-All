b9dd2c6bffec719c7b39595bb028e1e144c7e453
Lígia Bicalho <ligiabicalho@gmail.com>
2023-01-29 20:41:41 -0300

    feat 6 - Exibe o relatório de faturamento da empresa

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT 
+	MIN(p.valor) AS faturamento_minimo,
+  MAX(p.valor) AS faturamento_maximo,
+  ROUND(AVG(p.valor), 2) AS faturamento_medio,
+  SUM(p.valor) AS faturamento_total
+FROM SpotifyClone.planos AS p 
+INNER JOIN SpotifyClone.usuarios AS u ON p.plano_id = u.plano_id;
